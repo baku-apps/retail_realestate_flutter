d6efa7943d7099e6c848f8164cc57275f814e16d
B. Kuipers <bart_k2@hotmail.com>
2019-07-19 17:03:09 +0800

    Add google maps iOS support

--- ios/Runner/AppDelegate.m
+++ ios/Runner/AppDelegate.m
@@ -1,13 +1,13 @@
 #include "AppDelegate.h"
 #include "GeneratedPluginRegistrant.h"
+#import "GoogleMaps/GoogleMaps.h"
 
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application
     didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+  [GMSServices provideAPIKey:@"AIzaSyCUV61H8446XYwiQmzq-V6R9j_xQVMXgro"];
   [GeneratedPluginRegistrant registerWithRegistry:self];
-  // Override point for customization after application launch.
   return [super application:application didFinishLaunchingWithOptions:launchOptions];
 }
-
 @end
