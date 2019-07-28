6918e16896266f1f2e6a32a63af06c8494684f8d
B. Kuipers <bart_k2@hotmail.com>
2019-07-28 18:34:16 +0800

    Add iOS segments to detailsmaps and update pubs

--- lib/ui/detailView/DetailMapView.dart
+++ lib/ui/detailView/DetailMapView.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:retail_realestate_flutter/models/propertyDetails.dart';
@@ -32,11 +33,28 @@ class _DetailMapsViewState extends State<DetailMapsView> {
           position: LatLng(location[0], location[1]))
     }.toSet();
 
+    final segments =
+        Map<String, Widget>.fromIterable({"normal", "satelite", "terrein"},
+            key: (item) => item.toString(),
+            value: (item) => Container(
+                  padding: const EdgeInsets.all(4),
+                  child: Text(item.toString().toUpperCase(), 
+                  style: Theme.of(context).textTheme.subtitle.copyWith(color: Colors.white),),
+                ));
+
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.black,
         automaticallyImplyLeading: false,
         elevation: 0.1,
+        centerTitle: true,
+        title: CupertinoSegmentedControl<String>(
+          borderColor: Colors.white,
+          selectedColor: Colors.white,
+          unselectedColor: Colors.black,
+          children: segments,
+          onValueChanged: (map) => {},
+        ),
         actions: <Widget>[
           InkWell(
             child: Icon(Icons.cancel),
